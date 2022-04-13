ff73285d8c4c4b476509a0b82f3f72a3521f809b
Emily Xiong <xiongemi@gmail.com>
2022-04-13 14:53:13 -0400

    change to babel.config.js to make reanimated web work

--- apps/poetry-app/babel.config.js
+++ apps/poetry-app/babel.config.js
@@ -2,10 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [],
+    plugins: ['react-native-reanimated/plugin'],
     env: {
       production: {
-        plugins: ['react-native-paper/babel', 'react-native-reanimated/plugin'],
+        plugins: ['react-native-paper/babel'],
       },
     },
   };
