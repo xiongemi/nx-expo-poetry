25de4553610c200c2a4dee4a3c597871be30fc56
Emily Xiong <xiongemi@gmail.com>
2022-07-20 14:11:27 -0400

    upgrade expo to 14.4.4

--- apps/poetry-app-e2e/test-setup.ts
+++ apps/poetry-app-e2e/test-setup.ts
@@ -1,8 +1,8 @@
 import { device } from 'detox';
 
-import { expoDevClient } from './support/expo.utils';
+// import { expoDevClient } from './support/expo.utils';
 
 beforeAll(async () => {
   await device.launchApp();
-  await expoDevClient();
+  // await expoDevClient();
 });
