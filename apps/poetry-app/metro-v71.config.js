3be4e868b436de0ef7866418a0c3001919af44ed
Emily Xiong <xiongemi@gmail.com>
2024-02-14 15:37:47 -0500

    upgrade nx to 18

---
+++ apps/poetry-app/metro-v71.config.js
@@ -0,0 +1,29 @@
+/**
+ * Old custom configuration for React Native v0.71.
+ * From @react-native/metro-config 0.72.1, it is no longer necessary to use a config function to access the complete default config.
+ * Please port your custom configuration to metro.config.js.
+ * Please see https://docs.expo.dev/guides/customizing-metro/ to learn about configuration.
+ */
+const { getDefaultConfig } = require('@expo/metro-config');
+const { withNxMetro } = require('@nx/expo');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = (async () => {
+  defaultConfig.transformer.babelTransformerPath = require.resolve(
+    'react-native-svg-transformer'
+  );
+  defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(
+    (ext) => ext !== 'svg'
+  );
+  defaultConfig.resolver.sourceExts.push('svg');
+  return withNxMetro(defaultConfig, {
+    // Change this to true to see debugging info.
+    // Useful if you have issues resolving modules
+    debug: false,
+    // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx'
+    extensions: [],
+    projectRoot: __dirname,
+    watchFolders: [],
+  });
+})();
